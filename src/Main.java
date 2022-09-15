13243d818a8cb681f9bd83e55dd2b3fcc67473cc
HiImZsofi <zsofia.gyori00@gmail.com>
2022-09-15 09:11:24 +0200

    random coordinate generating method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import pont.Pont;
+
 public class Main {
     public static void main(String[] args) {
-
+        Pont p1 = new Pont();
+        Pont p2 = new Pont(0, 0);
+        Pont p3 = new Pont(3, 4);
     }
 }
--- src/pont/Pont.java
+++ src/pont/Pont.java
@@ -9,8 +9,23 @@ public class Pont {
         y = 0;
     }
 
+    public Pont(int n){
+        this.x = generateCoordinate(n);
+        this.y = generateCoordinate(n);
+    }
+
+    private static int generateCoordinate(int n) {
+        return (int) (Math.random() * ((2 * n) + 1)) - n;
+    }
+
     public Pont(int x, int y){
         this.x = x;
         this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        //return "Pont{" + "x=" + x + ", y=" + y + '}';
+        return String.format("(%d, %d)", this.x, this.y);
     }
 }
