b10dbd76c06c647e574f83b2aff5237266a00777
HiImZsofi <zsofia.gyori00@gmail.com>
2022-09-15 08:45:52 +0200

    object class

---
+++ src/pont/Pont.java
@@ -0,0 +1,16 @@
+package pont;
+
+public class Pont {
+    private int x;
+    private int y;
+
+    public Pont() {
+        x = 0;
+        y = 0;
+    }
+
+    public Pont(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
